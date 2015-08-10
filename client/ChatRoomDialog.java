7883b189a1389d5179416a1b09b5893c6780ac3b
apmavs <apmavs@users.noreply.github.com>
2015-08-10 03:00:44 -0500

    Pham -- Purdied up the GUI.

--- client/ChatRoomDialog.java
+++ client/ChatRoomDialog.java
@@ -16,29 +16,31 @@ import javax.swing.GroupLayout.*;
 //This class is the GUI that is the actual chatroom
 public class ChatRoomDialog extends JFrame
                            implements DocumentListener {
+    final static boolean shouldFill = true;
+    final static boolean shouldWeightX = true;
+    final static boolean RIGHT_TO_LEFT = false;
     
     private JTextField entry;
-    private JLabel jLabel1;
     private JButton sendButton;
     private JButton leaveButton;
-    private JScrollPane jScrollPane1;
     private JLabel status;
-    private JTextArea textArea;
+//    private JTextArea textArea;
     private String roomName;
     private String myName;
     static ClientInterface clientIF;
     private ArrayList users;
     private Iterator userIterator;
-    static JList userList;
+    static JList userList; // users
     static DefaultListModel userModel;
-    
-    
+    static JList chatlogList; // chat_log
+    static DefaultListModel chatlogModel;
+  
     final static Color  ERROR_COLOR = Color.PINK;
     final static String CANCEL_ACTION = "cancel-search";
     final static String SEND_ACTION = "send-message";
     
     final Color entryBg;    
-    
+
     //Constructor
     //creates the dialog
     public ChatRoomDialog(String incomingName, String incomingUserName) {
@@ -47,7 +49,7 @@ public class ChatRoomDialog extends JFrame
         initComponents();
         
         users = new ArrayList();
-        
+
         entryBg = entry.getBackground();
         entry.getDocument().addDocumentListener(this);
         
@@ -60,14 +62,21 @@ public class ChatRoomDialog extends JFrame
     //initComponents
     //initializes all the pieces of the GUI
     private void initComponents() {
-        entry = new JTextField();
-        textArea = new JTextArea();
+    	Container pane = getContentPane();
+
+    	JButton button;
+        JTextField textfield;
+        JLabel label;
+
+    	entry = new JTextField();
+//		textArea = new JTextArea();
         status = new JLabel();
-        jLabel1 = new JLabel();
         sendButton = new JButton("Send");
         leaveButton = new JButton("Leave Room");
 		userModel = new DefaultListModel();
 		userList = new JList(userModel);
+    	chatlogModel = new DefaultListModel();
+    	chatlogList = new JList(chatlogModel);
 
         setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
@@ -77,52 +86,96 @@ public class ChatRoomDialog extends JFrame
         });
         setTitle(roomName + " - " + myName);
 
-        textArea.setColumns(20);
+        getContentPane().setLayout(new GridBagLayout());        
+        GridBagConstraints c = new GridBagConstraints();
+		if (shouldFill) {
+    		//natural height, maximum width
+	    	c.fill = GridBagConstraints.HORIZONTAL;
+		}
+        
+/*        textArea.setColumns(20);
         textArea.setLineWrap(true);
         textArea.setRows(5);
         textArea.setWrapStyleWord(true);
         textArea.setEditable(false);
         jScrollPane1 = new JScrollPane(textArea);
-
-        jLabel1.setText("Enter text to send:");
-
-        getContentPane().setLayout(new GridBagLayout());
-        
-        GridBagConstraints c = new GridBagConstraints();
-        
-        c.weightx = 0.5;
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 0;
-        getContentPane().add(entry, c);
-        
-        c.weightx = 0.5;
+*/
+		// Messages Label
+    	label = new JLabel("Messages:");
+    	if (shouldWeightX) {
+    	c.weightx = 0.5;
+    	}
+    	c.fill = GridBagConstraints.HORIZONTAL;
+    	c.weightx = 0.5;
+    	c.gridx = 0;
+    	c.gridy = 0;
+    	pane.add(label, c);
+       
+    	// Leave Button
+        c.weightx = 0.0;
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 1;
         c.gridy = 0;
-        getContentPane().add(sendButton,c);
-		
-        c.weightx = 0.5;
+        pane.add(leaveButton, c);
+        		
+        // Members Label
+    	label = new JLabel("Members:");
+    	c.fill = GridBagConstraints.HORIZONTAL;
+    	c.weightx = 0.5;
+    	c.gridx = 2;
+    	c.gridy = 0;
+    	pane.add(label, c);
+
+        // Chat Log List
+        chatlogList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		JScrollPane jScrollPane2 = new JScrollPane();
+		jScrollPane2.setViewportView(chatlogList);
+        c.weightx = 0.0;
         c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 2;
-        c.gridy = 0;
-        getContentPane().add(leaveButton, c);
-        
-        c.weightx = 0.5;
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.ipady = 100;
+        c.ipady = 300;
         c.gridx = 0;
         c.gridy = 1;
         c.gridwidth = 2;
-        getContentPane().add(textArea,c );
+        getContentPane().add(jScrollPane2,c );
+		// scrolls to bottom of list	
+		int lastIndex = chatlogList.getModel().getSize() - 1;
+		if (lastIndex >= 0) {
+			chatlogList.ensureIndexIsVisible(lastIndex);
+		}
         
-        c.weightx = 0.5;
+        // User List
+        userList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        JScrollPane jScrollPane1 = new JScrollPane();
+		jScrollPane1.setViewportView(userList);
+        c.weightx = 0.0;
         c.fill = GridBagConstraints.HORIZONTAL;
-        c.ipady = 100;
+        c.ipady = 300;
         c.gridx = 2;
         c.gridy = 1;
-        getContentPane().add(userList, c);
+        getContentPane().add(jScrollPane1, c);
+    	
+    	// Entry Textbox
+    	c.fill = GridBagConstraints.HORIZONTAL;
+    	c.ipady = 0;       //reset to default
+    	//c.weighty = 1.0;   //request any extra vertical space
+    	c.anchor = GridBagConstraints.PAGE_END; //bottom of space
+    	//c.insets = new Insets(10,0,0,0);  //top padding
+    	c.gridx = 0;       //aligned with button 2
+    	c.gridwidth = 2;   //2 columns wide
+    	c.gridy = 2;       //third row
+    	pane.add(entry, c);
         
+    	// Send Button
+        c.fill = GridBagConstraints.HORIZONTAL;
+    	c.ipady = 0;       //reset to default
+    	//c.weighty = 1.0;   //request any extra vertical space
+    	c.anchor = GridBagConstraints.PAGE_END; //bottom of space
+    	//c.insets = new Insets(10,0,0,0);  //top padding
+    	c.gridx = 2;       //aligned with button 2
+//    	c.gridwidth = 1;   //2 columns wide
+    	c.gridy = 2;       //third row
+    	pane.add(sendButton, c);
+
         setPreferredSize(new Dimension(400,400));
 		pack();
 		setVisible(true);
@@ -155,7 +208,8 @@ public class ChatRoomDialog extends JFrame
         
         /* Send message to server */
         try {
-        	textArea.append(myName + " - " + s + "\n");
+        	//textArea.append(myName + " - " + s + "\n");
+        	chatlogModel.addElement(myName + " - " + s + "\n");
 			clientIF.sendMessage(myName + " - " + s + "\n", roomName);
 		} catch (RemoteException e) {
 			// TODO Auto-generated catch block
@@ -184,13 +238,8 @@ public class ChatRoomDialog extends JFrame
     //take the incoming message, and puts it in the main text area
     void displayIncomingMsg(String incomingMsg)
     {
-    	textArea.append(incomingMsg);
-    }
-        
-    //message
-    //displays a message at the bottom of the GUI
-    void message(String msg) {
-        status.setText(msg);
+    	//textArea.append(incomingMsg);
+    	chatlogModel.addElement(incomingMsg);
     }
     
     public void addUser(String userName)
@@ -217,6 +266,12 @@ public class ChatRoomDialog extends JFrame
     	userModel.removeElementAt(userModel.indexOf(userName));
     	users.remove(users.indexOf(userName));
 	}
+        
+    //message
+    //displays a message at the bottom of the GUI
+    void message(String msg) {
+        status.setText(msg);
+    }
 
     // DocumentListener methods
     
@@ -248,7 +303,7 @@ public class ChatRoomDialog extends JFrame
     }
     
     //SendAction
-    //listender for the send button
+    //listener for the send button
     class SendAction extends AbstractAction {
         public void actionPerformed(ActionEvent ev) {
         	send();
@@ -261,5 +316,5 @@ public class ChatRoomDialog extends JFrame
 	{
 		clientIF = incomingIF;
 	}
-   
+
 }
